736cb26e9c581f49c6b4c5ad16e6cd02895e51b7
Jeet Chaani <jeetchaani23@gmail.com>
2023-10-25 21:22:17 +0530

    Todo localStorage

--- 8 ToDo List Using React/todo/src/components/Mainpart/Main.jsx
+++ 8 ToDo List Using React/todo/src/components/Mainpart/Main.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useTodo } from "../../context/todo";
+import './main.css';
 export default function Main() {
   const state = useTodo();
-  const { title, setTitle } = state;
+  const { title, setTitle, task, setTask } = state;
+  const addTodo = () => {
+    //check for empty
+    if (title !== "") {
+      //store to local storage
+      setTask([...task, { title: title, date: Date.now() }]);
+
+      setTitle("");
+    }
+  };
+  const deleteTodo = (id) => {
+    const updateTodo = task.filter((currItem) =>{
+      return currItem.date!==id;
+    });
+    setTask(updateTodo);
+  };
+  useEffect(() => {
+    localStorage.setItem("todo", JSON.stringify(task));
+  }, [task]);
   return (
     <div>
       <input
@@ -10,8 +29,16 @@ export default function Main() {
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
-      <button>Add</button>
-      <p>{title}</p>
+      <button onClick={addTodo}>Add</button>
+      <ul className="item-list">
+        {task.map((item, index) => {
+          return (
+            <li key={index}>
+              {item.title} <span onClick={()=>deleteTodo(item.date)} className="delete-item">-</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
--- 8 ToDo List Using React/todo/src/context/todo.jsx
+++ 8 ToDo List Using React/todo/src/context/todo.jsx
@@ -2,10 +2,20 @@ import { createContext, useContext, useState } from "react";
 
 const TodoContext = createContext();
 
+const todoData = () => {
+  const data = localStorage.getItem("todo");
+  if (data) {
+    return JSON.parse(data);
+  } else {
+    return [];
+  }
+};
+
 const TodoProvider = (props) => {
   const [title, setTitle] = useState("");
+  const [task, setTask] = useState(todoData());
   return (
-    <TodoContext.Provider value={{title, setTitle}}>
+    <TodoContext.Provider value={{ title, setTitle, task, setTask }}>
       {props.children}
     </TodoContext.Provider>
   );
